bcd63e63d5c2e876d66bac7a31a7018ca148db77
Retkowsky <vincentr94@icloud.com>
2021-10-22 02:33:17 +0200

    commit V.final of the project 1

--- proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
@@ -2,19 +2,48 @@ package agh.ii.prinjava.proj1;
 
 import agh.ii.prinjava.proj1.impl.MyQueueDLLBImpl;
 
+/**
+ * interface of Myqueue
+ * @param <E>
+ */
 public interface MyQueue<E> {
+    /**
+     * add a node a the start of the queue
+     * @param x
+     */
     void enqueue(E x);
 
+    /**
+     * remove the last node of the queue
+     */
     void dequeue();
+
+    /**
+     * it return the first node of the queue
+     * @return the first node of the queue
+     */
     E returnFirstQueue();
+
+    /**
+     * it return the last node of the queue
+     * @return the last node of the queue
+     */
     E returnLastQueue();
 
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     *
+     * @return the number of nodes in the queue
+     */
     int numOfElems();
 
+    /**
+     *
+     * @return the last nodes in the queue
+     */
     E peek();
 
     /** Consider pros and cons of having a factory method in the interface */
--- proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
@@ -2,11 +2,33 @@ package agh.ii.prinjava.proj1;
 
 import agh.ii.prinjava.proj1.impl.MyStackDLLBImpl;
 
+/**
+ *interface for MyStack
+ * @param <E>
+ */
 public interface  MyStack<E> {
+    /**
+     *it remove the first element of a stack
+     */
     void pop();
 
+    /**
+     *it add an element at the start of a stack
+     * @param x
+     */
     void push(E x);
+
+    /**
+     *it return the data of the first node
+     * @return the data of the first node
+     */
     E returnFirstStack();
+
+    /**
+     *it return the data of the last node
+     * @return the data of the last node
+     */
+
     E returnLastStack();
 
 
@@ -15,8 +37,16 @@ public interface  MyStack<E> {
         return numOfElems() == 0;
     }
 
+    /**
+     *it return the number of nodes
+     * @return the number of nodes
+     */
     int numOfElems();
 
+    /**
+     *it return the ifrst element of the list
+     * @return the first node of the list
+     */
     E peek();
 
     /** Consider pros and cons of having a factory method in the interface */
